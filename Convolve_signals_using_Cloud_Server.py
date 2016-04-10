cc0e64deb86ec5879eb436f721bbc38983318193
Nimish Joshi <jnimish77@users.noreply.github.com>
2016-04-10 03:04:06 -0500

    Create Convolve_signals_using_Cloud_Server.py
    Convolve two given signals (both are Asin(ωt)) and plot.
    Signal 1: Amplitude: A1 = 10
    Frequency: ω1 = 2Hz
    Signal 2: Amplitude: A2 = .1
    Frequency: ω2 = 20Hz
    Time: t defined as: start = 0, end = 10, step = .01
    Plot Directions:
    In one figure, use subplots to plot the following 3 signals, Signal 1, Signal 2 and the signal
    resulting from convolution of Signals 1 & 2.
    Be sure to extract the last half of the resulting convolution to force the final plot to be of the
    same length as Signals 1 & 2.
    Save resulting plot as a PDF file.
    Leave both code and output file in /home/student/homework/hw3/ directory. Name code file
    hw3.py and output as hw3_out.pdf.
    
    Hint: Lookup pyplot from MatPlotLib in the online docs.

---
+++ Convolve_signals_using_Cloud_Server.py
@@ -0,0 +1,59 @@
+#########################################################
+#                                                       #
+#  Nimish Joshi                                         #
+#  10/05/2015                                           #
+#  Program to Convolve given two signls    				#
+#   and observing its output                            #
+#                                                       #
+#########################################################
+
+import numpy as np          #importing np as numpy library
+import matplotlib.pyplot as plt   #importing library for plotting
+from matplotlib.backends.backend_pdf import PdfPages  #importing library for pdf conversion
+
+
+t = np.arange(0, 10, 0.01) # defining starting and ending value of scale
+s1 = 10*np.sin(2*t)
+s2 = 0.1*np.sin(20*t)
+
+res = np.convolve(s1, s2)   #convolution result of two signal
+x1 = np.zeros(shape=(0,1000))   #it takes off the the second half of the signl
+x1= res[0:1000]                 #defining limit of the result that we want to show
+
+
+x2= np.zeros(shape=(1000,))  # takes off the first half of the result
+x2 = res[999:1999]
+
+#plotting the graphs
+
+plt.subplot(411)
+plt.plot(t,s1)
+plt.title('signl 1')
+plt.grid(True)
+
+plt.subplot(412)
+plt.plot(t,s2)
+plt.ylim(-0.10, 0.15)
+plt.title('signal 2')
+
+plt.grid(True)
+               
+plt.subplot(413)
+plt.plot(t,x1)
+plt.ylim(-8, 8)
+plt.title('1st half of the convolution')
+
+plt.subplot(414)
+plt.plot(t,x2)
+plt.title('Last Half of the Convolution')
+
+plt.subplots_adjust(hspace=.4)  # to adjust space between two graphs
+#plt.tight_layout()
+plt.show()
+
+# pp = PdfPages('hw3_out.pdf')  # converts the figure/output into the pdf
+# pp.savefig(a)
+# pp.close()
+
+                  
+
