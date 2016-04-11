700779470fde1a3da78a2906efb0e32d506a27d8
Nimish Joshi <jnimish77@users.noreply.github.com>
2016-04-11 02:06:33 -0500

    Update code_2.py

--- MPI_Practice_Examples/code_2.py
+++ MPI_Practice_Examples/code_2.py
@@ -24,6 +24,7 @@ else:
 
 """ Note: Send and Recv are referred to as blocking functions. That is, if a process calls Recv, it will sit idle until it has received a message from a corresponding Send before it will proceeed.
 If it did not receive its message it will indicate to the system that it will be receiving a message, proceed beyond theIrecv to do other useful work, and then check back later to see if the message has arrived. This can be used to dramatically improve performance
+
 On a Recv you do not always need to specify the source. Instead, you can allow the calling process to accept a message from any process that happend to be sending to the receiving process. This is done by setting source to a predefined MPI constant, source=ANY_SOURCE (note that you would first need to import this with from mpi4py.MPI import ANY_SOURCE or use the syntax source=MPI.ANY_SOURCE).
 
 """
